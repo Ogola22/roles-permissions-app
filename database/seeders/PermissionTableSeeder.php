86e5a70c9331e98cd4de521c2eab5eb4e69902d5
brian_ogola <brianochiengogola78@gmail.com>
2023-03-07 23:45:55 +0300

    admin seeder

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Spatie\Permission\Models\Permission;
+
 class PermissionTableSeeder extends Seeder
 {
     /**
@@ -12,6 +14,20 @@ class PermissionTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //Permissions
+        $permissions = [
+            'role-list',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'product-list',
+            'product-create',
+            'product-edit',
+            'product-delete'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
     }
 }
